462bb8e5fcf2481d04e162b6fdfb63cc8c998477
CalosVds <carloseduardovds@gmail.com>
2021-05-07 22:58:31 -0300

    Integração BarChart

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -25,12 +25,6 @@ type ChartData = {
                 setChartData({ series:mySeries, labels:myLabels});
             })
         }, []);
-
-       
-        //const mockData = {
-            //series: [477138, 499928, 444867, 220426, 473088],
-            //labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-       // }
         
         const options = {
             legend: {
